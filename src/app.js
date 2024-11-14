d9c76536a6dea41b7d8221bcdeebcbb679937090
valeriagiron <valeriagironv@gmail.com>
2024-11-13 22:13:22 -0500

    change imports

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from "express";
 
 // Importación de enrutadores
-import routerRecipes from "./routes/recipe.router.js";
-import routerIngredients from "./routes/ingredient.router.js";
+import routerCart from "./routes/cart.router.js";
+import routerProducts from "./routes/product.router.js";
 
 // Se crea una instancia de la aplicación Express
 const app = express();
@@ -21,8 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Declaración de rutas
-app.use("/api/recipes", routerRecipes);
-app.use("/api/ingredients", routerIngredients);
+app.use("/api/products", routerProducts);
+app.use("/api/Cart", routerCart);
 
 // Se levanta el servidor oyendo en el puerto definido
 app.listen(PORT, () => {
